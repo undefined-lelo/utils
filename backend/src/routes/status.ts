ec312cb9cc156b073c817d1fae0a9b762024e799
lezetho <root@lezetho.xyz>
2026-02-17 12:53:01 -0500

    add ver to status

--- backend/src/routes/status.ts
+++ backend/src/routes/status.ts
@@ -1,9 +1,10 @@
 import type { FastifyInstance } from 'fastify';
-
+import {version} from '../../package.json'
 export default async function statusRoutes(fastify: FastifyInstance) {
     fastify.get('/status', async (request, reply) => {
         return { 
             status: 'up',
+            version: version,
             'uptime-seconds': process.uptime(),
             message: 'did you know that this worked? :wow:'
         };
